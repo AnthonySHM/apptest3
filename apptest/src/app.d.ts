7f19b0521ef88dea8905bed31531f962e8ecd52f
Anthony <anthonyshiloo04@gmail.com>
2024-07-17 16:35:19 +0300

    second

---
+++ apptest/src/app.d.ts
@@ -0,0 +1,27 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
+
+declare global {
+	interface Response {
+		products : Product[]
+	}
+
+	interface Product {
+		id : string
+		title : string
+		thumbnail :  string 
+		description : string
+
+	}
+}
+
+export {};
